b80b498477e618f9ce2f15d08ff64bf3c2fde749
Alexander Alemayhu <alexander@alemayhu.com>
2023-09-14 20:29:32 +0200

    fix: get the transforming working

--- src/csv-to-apkg.test.ts
+++ src/csv-to-apkg.test.ts
@@ -1,7 +1,8 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { describe, it, expect, fail } from 'vitest';
+import { describe, expect, it } from 'vitest';
 
-import convertCSVToAPKG, { CSVReader, DataMapper } from './csv-to-apkg';
+import convertCSVToAPKG, { createDeck, readCSVContent } from './csv-to-apkg';
+import get16DigitRandomId from './get16DigitRandomId';
 
 const CSV_EXAMPLE = `
 Word,Meaning,Tags
@@ -10,36 +11,58 @@ Word,Meaning,Tags
 ありがとうございます。,Thank you (polite),three
 `;
 
-describe("Convert CSV to APKG", () => {
-  it.skip("converts CSV to APKG format", () => {
-    expect(convertCSVToAPKG()).toContain("magic")
+describe('Convert CSV to APKG', () => {
+  it.skip('converts CSV to APKG format', () => {
+    expect(convertCSVToAPKG())
+      .toContain('magic');
   });
-  
-  it.skip("reads the CSV content correctly", () => {
-    const reader = new CSVReader(CSV_EXAMPLE);
-    expect(reader.getHeaders()).toBe(['Word', 'Meaning', 'Tags']);
-    expect(reader.getRows()).toBe(
-      [
-        ['お願いします', 'Please', 'one'],
-        ['ありがとう', 'Thank you (impolite)', 'two'],
-        ['ありがとうございます。', 'Thank you (polite)', 'three']
-      ]
-    )
-  });
-  
-  it.skip("Transform to decks / notes", () => {
-    const reader = new CSVReader(CSV_EXAMPLE);
-    const mapper = new DataMapper(reader.parseSync())
-    expect(mapper.getDecks()).toBe(['Default'])
-  });
-  
-  it.skip("uses filename as deck name", () => {
-    console.info("to be implemented");
-  })
-  
-  it.skip("uses custom filename as deck name", () => {
-    console.info("to be implemented");
-  })
-  
-})
 
+  it('reads the CSV content correctly', () => {
+    const {
+      headers,
+      rows,
+    } = readCSVContent(CSV_EXAMPLE);
+
+    expect(headers)
+      .toEqual(['Word', 'Meaning', 'Tags']);
+    expect(rows)
+      .toEqual(
+        [
+          ['お願いします', 'Please', 'one'],
+          ['ありがとう', 'Thank you (impolite)', 'two'],
+          ['ありがとうございます。', 'Thank you (polite)', 'three'],
+        ],
+      );
+  });
+
+  it('Transform to decks / notes', () => {
+    const data = readCSVContent(CSV_EXAMPLE);
+    const deck = createDeck({
+      name: 'Default',
+      cards: [],
+      image: undefined,
+      style: null,
+      id: get16DigitRandomId(),
+      data,
+    });
+
+    expect(deck.name)
+      .toEqual('Default');
+    expect(
+      deck.cards[0].name,
+    )
+      .toEqual('お願いします');
+    expect(
+      deck.cards[0].back,
+    )
+      .toEqual('Please one');
+  });
+
+  it.skip('uses filename as deck name', () => {
+    console.info('to be implemented');
+  });
+
+  it.skip('uses custom filename as deck name', () => {
+    console.info('to be implemented');
+  });
+});
--- src/csv-to-apkg.ts
+++ src/csv-to-apkg.ts
@@ -1,46 +1,129 @@
 export default function convertCSVToAPKG(): string | null {
-  throw new Error("To be implemented")
+  throw new Error('To be implemented');
 }
 
-
 export type CSVColumnHeader = string
-export type CSVRow = string
+export type CSVRow = string[]
 export type CSVData = {
   headers: CSVColumnHeader[];
   rows: CSVRow[];
 }
 
-export class CSVReader {
-  
-  constructor(readonly content: string) { }
-  
-  getHeaders(): CSVColumnHeader[] {
-    throw new Error("not implemented")
-  }
-  
-  getRows(): CSVRow[] {
-    throw new Error("not implemented")
-  }
-  
-  parseSync(): CSVData {
-    throw new Error("To be implemented")
-  }
+function skipFirstLine(lines: string[]): string[] {
+  return lines.slice(1);
 }
 
-export type Note = {
-  id: number;
-  front: string;
-  back: string;
-  tags: string;  
+function getFields(line: string, delimiter: string = ',') {
+  return line.split(delimiter);
 }
 
-export type Deck = {
-  subDecks: Deck[];
+function getTrimmedLines(content: string): string[] {
+  return content.trim()
+    .split('\n');
+}
+
+function getFirstLine(lines: string[]) {
+  return lines[0];
+}
+
+export function readCSVContent(content: string): CSVData {
+  const lines = getTrimmedLines(content);
+  const headers = getFields(getFirstLine(lines));
+  const rows = skipFirstLine(lines)
+    .map((line) => getFields(line));
+
+  return {
+    headers,
+    rows,
+  };
+}
+
+/**
+ * https://github.com/2anki/server/blob/main/src/lib/parser/Note.ts
+ */
+class Note {
   name: string;
-  notes: Note[];
+
+  back: string;
+
+  tags: string[];
+
+  cloze = false;
+
+  number = 0;
+
+  enableInput = false;
+
+  answer = '';
+
+  media: string[] = [];
+
+  notionId?: string;
+
+  notionLink?: string;
 }
-export class DataMapper {
-  getDecks(): Deck {
-    throw new Error("To be implemented")
+
+/**
+ * https://github.com/2anki/server/blob/main/src/lib/parser/Deck.ts
+ */
+class Deck {
+  name: string;
+
+  cards: Note[];
+
+  image: string | undefined;
+
+  style: string | null;
+
+  id: number;
+
+  constructor(
+    name: string,
+    cards: Note[],
+    image: string | undefined,
+    style: string | null,
+    id: number,
+  ) {
+    this.name = name;
+    this.cards = cards;
+    this.image = image;
+    this.style = style;
+    this.id = id;
   }
+}
+
+interface CreateDeckOptions extends Deck {
+  data: CSVData;
+}
+
+function locateCards(data: CSVData): Note[] {
+  return data.rows.map((row) => {
+    const fields = getFields(row.toString());
+    const name = getFirstLine(fields);
+    const back = skipFirstLine(fields)
+      .join(' ');
+
+    return {
+      name,
+      back,
+      tags: [],
+      cloze: false,
+      number: 0,
+      enableInput: false,
+      answer: '',
+      media: [],
+    };
+  });
+}
+
+export function createDeck({
+  name,
+  cards,
+  image,
+  style,
+  id,
+  data,
+}: CreateDeckOptions): Deck {
+  const newCards = locateCards(data);
+  return new Deck(name, [...cards, ...newCards], image, style, id);
 }
